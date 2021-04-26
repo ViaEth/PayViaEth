823ba7265fbaaa994d0eebb76b04511c8b0b8e90
T. Thomas <dev@viaeth.io>
2021-04-25 19:47:20 -0700

    1. Updated license to match other locations.

--- cms90-woocommerce-ethereum-payment.php
+++ cms90-woocommerce-ethereum-payment.php
@@ -5,7 +5,7 @@ Plugin URI:  https://viaeth.io
 Description: Woocommerce Ethereum Payment Plugin
 Version:     0.2.5
 Author:      Tyler Thomas(https://www.SpaceAgeMinds.com), Xufeng Wang(http://www.upwork.com/fl/albertw6)
-License:     GPL3
+License:     GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Domain Path: /languages
 Text Domain: c9wep
