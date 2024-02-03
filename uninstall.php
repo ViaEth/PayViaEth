1cd058c42b7429dffb7d71708f4250eda676accb
Via.Eth <contact+dev@viaeth.io>
2024-02-02 21:28:08 -0800

    Added code to drop pve specific database tables

--- uninstall.php
+++ uninstall.php
@@ -1,13 +1,21 @@
 <?php
 //https://developer.wordpress.org/plugins/the-basics/uninstall-methods/
-// If uninstall is not called from WordPress, exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
 }
 
-// $option_name = 'plugin_option_name';
- 
-// delete_option( $option_name );
- 
-// // For site options in Multisite
-// delete_site_option( $option_name );  
+// remove plugin options
+//$option_name = 'pve_option';
+
+//delete_option( $option_name );
+
+// remove plugin transients
+
+// remove plugin cron events
+
+// ..etc., based on what needs to be removed
+
+// drop plugin specific database tables
+global $wpdb;
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}c9wep_ethereum_payments" );
