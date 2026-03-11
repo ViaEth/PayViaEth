58ea06255556f32a51cdcef53cce7c0ef65437f9
Dev @ MeaningfulBits.io <contact+dev@meaningfulbits.io>
2026-03-10 22:23:27 -0700

    Started updating the uninstall file to remove settings and other info using standard wp functions

--- uninstall.php
+++ uninstall.php
@@ -1,26 +1,25 @@
 <?php
 //https://developer.wordpress.org/plugins/the-basics/uninstall-methods/
 // if uninstall.php is not called by WordPress, die
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die;
-}
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
 // remove plugin options
-$wp_wc_pve_options = array('woocommerce_ethereumpay_settings', 'c9wep_ethereum_payments_db_version');
+$wp_wc_pve_options = array('woocommerce_ethereumpay_settings', 'pve_ethereum_payments_db_version');
 foreach ( $wp_wc_pve_options as $option ) {
 	delete_option( $option );
 }
 
-// remove plugin transients
+// remove plugin options
+delete_option( 'pve_eth_price_history' );
+delete_option( 'pve_eth_fetch_failures' );
 
-// remove plugin cron events
+// remove plugin transients
+delete_transient( 'pve_eth_usd_price' );
+
+// WooCommerce gateway plugin
+delete_option( 'woocommerce_pve_gateway_settings' );
 
 // remove plugin logs
 
 // ..etc., based on what needs to be removed
-
-// drop plugin specific database tables
-global $wpdb;
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}c9wep_ethereum_payments" );
-
 
