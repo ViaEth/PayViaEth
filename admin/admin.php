e2eb5ea3e310b0d18211bb0775c6ff42cb63f2df
Dev @ MeaningfulBits.io <contact+dev@meaningfulbits.io>
2026-03-15 00:31:39 -0700

    Removed old code and unused folders and functions.

--- admin/admin.php
+++
@@ -1,3 +0,0 @@
-<?php 
-require_once dirname(__FILE__) . '/class-settings.php';
-require_once PVE_DIR . '/admin/ajax/check_connection/check_connection.php';
--- ethereum_payments/call-sample-ethereum_payments-init.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once C9WEP_DIR . '/includes/form-fields.php';
-require_once C9WEP_DIR . '/includes/db-functions.php';
-require_once C9WEP_DIR . '/ethereum_payments/ethereum_payments-init.php';
