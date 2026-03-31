ab8d6176f8bb49e93457f21176379687b8574483
Dev @ MeaningfulBits.io <contact+dev@meaningfulbits.io>
2026-03-30 20:58:27 -0700

    Remove Plugin Options is now a foreach loop, Removed logs. Logs are currently written to the wrong place and should be updated at some point.

--- uninstall.php
+++ uninstall.php
@@ -3,23 +3,29 @@
 // if uninstall.php is not called by WordPress, die
 defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
-// remove plugin options
-$wp_wc_pve_options = array('woocommerce_ethereumpay_settings', 'pve_ethereum_payments_db_version');
-foreach ( $wp_wc_pve_options as $option ) {
+//Remove Plugin Options
+$options = array(
+	'pve_eth_price_history',
+	'pve_eth_fetch_failures',
+	'woocommerce_pve_gateway_settings'//WooCommerce gateway plugin settings
+);
+foreach ( $options as $option ) {
 	delete_option( $option );
 }
 
-// remove plugin options
-delete_option( 'pve_eth_price_history' );
-delete_option( 'pve_eth_fetch_failures' );
+//Remove Plugin Transients
+delete_transient( 'pve_eth_usd_price' ); //Should be a for each loop at some point
 
-// remove plugin transients
-delete_transient( 'pve_eth_usd_price' );
-
-// WooCommerce gateway plugin
-delete_option( 'woocommerce_pve_gateway_settings' );
-
-// remove plugin logs
-
+//Remove Plugin Logs (At some point pve logs should be moved and stored in wp-content/logs/pve/)
+$log_dir = WP_CONTENT_DIR . '/uploads/pve-logs/';
+if ( is_dir( $log_dir ) ) {
+    $files = glob( $log_dir . '*.log' );
+    if ( $files ) {
+        foreach ( $files as $file ) {
+            wp_delete_file( $file );
+        }
+    }
+    rmdir( $log_dir );
+}
 // ..etc., based on what needs to be removed
 
